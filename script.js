4ae9c837558a70657ac8c02f08a37d7b441c570c
Fahad <fahadanwar5925@gmail.com>
2025-10-29 11:23:25 +0530

    Initial commit: Create basic HTML and button functionality

---
+++ script.js
@@ -0,0 +1,7 @@
+// Select the button from the HTML
+const checkButton = document.getElementById('check-btn');
+
+// Add an event listener that runs a function when the button is clicked
+checkButton.addEventListener('click', () => {
+  console.log('Button Clicked!');
+});
