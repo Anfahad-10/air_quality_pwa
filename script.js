415b27183ab1c049c5e6a0df00e4ecb775472ff6
Fahad <fahadanwar5925@gmail.com>
2025-10-30 23:29:41 +0530

    Update API URLs for production deploymenttt

--- script.js
+++ script.js
@@ -85,7 +85,7 @@ async function subscribeUserToPush() {
 }
 
 function fetchAirQualityFromServer(location) {
-  const serverUrl = `https://airquality-9j12muhi4-fahads-projects-3366c528.vercel.app/`;
+  const serverUrl = `https://breathquality.vercel.app/air-quality?lat=${latitude}&lon=${longitude}`;
   fetch(serverUrl)
     .then(response => response.json())
     // script.js ... inside fetchAirQualityFromServer
@@ -165,7 +165,7 @@ function sendSubscriptionToServer(subscription, location) {
   const frequency = dropdown.dataset.selectedValue || '28800000';
   console.log(`With selected frequency: ${frequency}ms`);
 
-  fetch('https://airquality-9j12muhi4-fahads-projects-3366c528.vercel.app/subscribe', {
+  fetch('https://breathquality.vercel.app/subscribe', {
     method: 'POST',
     body: JSON.stringify({ subscription, location, frequency }),
     headers: { 'Content-Type': 'application/json' }
