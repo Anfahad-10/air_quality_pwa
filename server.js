b646b80528ff9ff3b3546247232534564ae2fc7f
Fahad <fahadanwar5925@gmail.com>
2025-10-31 00:43:05 +0530

    Use absolute path joining to serve static files

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const fetch = require('node-fetch');
@@ -39,10 +40,10 @@ const port = 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static('.')); 
+app.use(express.static(path.join(__dirname, '.')));
 
 app.get('/', (req, res) => {
-  res.sendFile('index.html', { root: __dirname });
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 
